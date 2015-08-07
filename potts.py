30f5efc71971ba55108b866bff7088367a0d3aa7
Long Tran <v.long128@gmail.com>
2015-08-06 21:26:40 -0700

    renamed db. Ready to start inserting data.

--- potts.py
+++ potts.py
@@ -1,26 +1,31 @@
 from flask import Flask, jsonify, render_template, request
 from flaskext.mysql import MySQL
 
-mysql = MySQL()
 app = Flask(__name__)
 app.debug = True
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'potts'
-app.config['MYSQL_DATABASE_DB'] = 'PottsUsers'
+app.config['MYSQL_DATABASE_DB'] = 'Potts'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
 mysql.init_app(app)
+cursor = mysql.connect().cursor()
 
 @app.route("/authenticate")
 def authenticate():
     username = request.args.get('usr')
     password = request.args.get('pwd')
-    cursor = mysql.connect().cursor()
+    
     cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
     data = cursor.fetchone()
     if data is None:
     	return jsonify(result={"status":"failed"})
     else:
     	return jsonify(result={"status":"ok"})
+
+@app.route("/insert")
+def insert():
 
 @app.route("/")
 def index():
